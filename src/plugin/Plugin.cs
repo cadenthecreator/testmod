db92999d1d43effcace371a507cd76b5771c0f27
Caden Ream <114967401+cadenthecreator@users.noreply.github.com>
2024-04-27 20:21:43 -0700

    Initial commit

---
+++ src/plugin/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace ExamplePlugin
+{
+    [BepInPlugin("com.example.examplemod", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
